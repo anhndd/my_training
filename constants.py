671e4ba83afdd15c13fedd096d70212625c85243
anhndd <anhndd1510@gmail.com>
2019-04-27 11:36:31 +0700

    fix

--- constants.py
+++ constants.py
@@ -1,5 +1,5 @@
 memory_size = 20000								# 20.000
-mini_batch_size = 5							# 64
+mini_batch_size = 64							# 64
 a_dec = 4.5
 num_of_phase = 2
 action_policy = [[0, 0], [5, 0], [-5, 0], [0, 5], [0, -5]]  
@@ -8,7 +8,7 @@ idLightControl = '4628048104'
 
 
 # ==============
-Num_Exploration = 10						# exploring in: 2.000 steps.
+Num_Exploration = 2000						# exploring in: 2.000 steps.
 Num_Training	= 30000						# training in: 30.000 steps
 Num_Testing     = 250000
 
